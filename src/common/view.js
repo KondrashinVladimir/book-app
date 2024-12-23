864b576213013c645652c08d27bec42acc62ae68
Vladimir Kondrashin <electric379128@gmail.com>
2024-12-23 20:41:43 +0300

    initial commit

---
+++ src/common/view.js
@@ -0,0 +1,17 @@
+export class AbstractView {
+    constructor() {
+        this.app = document.getElementById('app');
+    }
+
+    setTitle(title) {
+        document.title = title;
+    }
+
+    render() {
+        return;
+    }
+
+    destroy() {  
+        return;
+    }
+}
